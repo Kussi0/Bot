d75998ae34124b8425f27ad19fa5bc69e8c1f041
Kussi <kayavl123@gmail.com>
2023-03-27 17:31:21 +0200

    dsadsa

--- src/bot.js
+++ src/bot.js
@@ -25,5 +25,27 @@ client.on('messageCreate', (message) => {
     }
 })
 
+module.exports.run = async (bot, message, args) => {
+
+if(!args[2]) return message.replay("Please ask a question!🍆");
+
+let replies = ["🎱 | It is certain", "🎱 | It is decidedly so", "🎱 | Without a doubt", "🎱 | Yes definitely", "🎱 | You may rely on it", 
+"🎱 | As I see it, yes", "🎱 | Most likely", "🎱 | Outlook good", "🎱 | Yes", "🎱 | Signs point to yes", "🎱 | Reply hazy, try again", 
+"🎱 | Ask again later", "🎱 | Better not tell you now", "🎱 | Cannot predict now", "🎱 | Concentrate and ask again", "🎱 | Don't count on it",
+"🎱 | My reply is no", "🎱 | My sources say no", "🎱 | Outlook not so good", "🎱 | Very doubtful"];
+
+let result = Math.floor((Math.random() * replies.lenght));
+let question = args.slice(1).join(" ");
+
+let ballembed = new DiscordAPIError.RichEmbed()
+.setAuthor(message.author.tag)
+.setcolor("#FF990")
+.addFeild("Question", question)
+.addFeild("Answer", replies[result]);
+
+message.channel.send(ballembed)
+
+}
+
 client.login(process.env.TOKEN);
 
